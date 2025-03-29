d1ee81760556858dcdee2f0facefbde0ced77605
Filipe Ferreira <filipeferreira.psid2019@gmail.com>
2025-03-29 10:34:50 -0300

    adicionando validações no FRONT

--- src/app/components/estado/estado-form/estado-form.component.ts
+++ src/app/components/estado/estado-form/estado-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { EstadoService } from '../../../services/estado.service';
 import { ActivatedRoute, Router,RouterModule} from '@angular/router';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,6 +9,7 @@ import { NgIf } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { Estado } from '../../../models/estado.model';
+import { HttpErrorResponse } from '@angular/common/http';
 
 
 
@@ -32,33 +33,77 @@ export class EstadoFormComponent {
       const estado: Estado = this.activatedRoute.snapshot.data['estado'];
       this.formGroup = this.formBuilder.group({
         id: [(estado && estado.id) ? estado.id : null],
-        nome: [(estado && estado.nome) ? estado.nome:'', Validators.required],
-        sigla: [(estado && estado.sigla) ? estado.sigla: '', Validators.required]
+        nome: [(estado && estado.nome) ? estado.nome:'', Validators.compose([Validators.required,Validators.maxLength(60)])],
+        sigla: [(estado && estado.sigla) ? estado.sigla: '',Validators.compose([Validators.required,Validators.maxLength(2)])]
       })
     }
 
     salvar(){
+      this.formGroup.markAllAsTouched();
       if( this.formGroup.valid){
         const estado = this.formGroup.value;
-        if (estado.id == null){
-        this.estadoService.salvar(estado).subscribe({
-          next: (estadoCadastrado) => {
+
+        const operacao = estado.id == null
+        ? this.estadoService.salvar(estado)
+        : this.estadoService.update(estado);
+
+        operacao.subscribe({
+        next: () => {
             this.router.navigateByUrl('/estados');
           },
           error: (errorResponse) => {
             console.log('Erro ao incluir' + JSON.stringify(errorResponse));
+            this.tratarErros(errorResponse);
           }
-        });
-        } else {
-          this.estadoService.update(estado).subscribe({
-            next: () => {
-              this.router.navigateByUrl('/estados');
-            },
-            error: (err) => {
-              console.log('Erro ao incluir' + JSON.stringify(err));
-            }
-          });
+        })
+  
+       
+      }
+    }
+
+    tratarErros(httpError: HttpErrorResponse):void{
+
+        if(httpError.status === 400) {
+          if(httpError.error?.errors){
+            httpError.error.errors.forEach((validationError: any) => {
+                const formControl = this.formGroup.get(validationError.fieldName);
+                if(formControl){
+                  formControl.setErrors({apiError: validationError.message});
+                }
+              });
+          };
+        }else{
+          alert(httpError.error?.message || "Erro não mapeado do servidor. ");
+        }
+    } 
+
+    getErrorMessage(controlName: string, errors: ValidationErrors | null | undefined): string {
+      if(!errors || !this.errorMessages[controlName]){
+        return 'invalid field';
+      }
+
+      for(const errorName in errors){
+        if(this.errorMessages[controlName][errorName]){
+          return this.errorMessages[controlName][errorName];
         }
       }
+      return 'invalid field';
+    }
+// é proximo ao map do java 
+    errorMessages: {[controlName: string] : {[errorName: string]: string}} = {
+      nome: {
+        required: 'O nome deve ser informado. ',
+        minlength: 'O nome deve ter no mínimo 2 caracteres. ',
+        maxLength: 'O nome deve ter no máximo 60 caracteres',
+        apiError: ' '
+      },
+      sigla: {
+        required: 'A sigla deve ser informada. ',
+        minlength: 'A sigla deve ter no mínimo 2 caracteres. ',
+        maxlength: 'A sigla deve ter no máximo 2 caracteres',
+        apiError: ' '
+      },
+
+
     }
 }
